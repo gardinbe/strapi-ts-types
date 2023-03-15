e60949ea63c421215add8d5dedff124677f31cf2
gardinbe <55744452+gardinbe@users.noreply.github.com>
2023-03-15 22:06:37 +0000

    update

--- strapi.d.ts
+++ strapi.d.ts
@@ -1,52 +1,59 @@
-export type StrapiResponse<T = {}> = StrapiPublishableItem<T> //response is a generic item, containing items within it
-
-export type StrapiItem<T> = {
-	data: {
-		id: number,
-		attributes: {
-			createdAt: string,
-			updatedAt: string
-		} & T
-	},
-	meta: {}
-}
-
-export type StrapiPublishableItem<T = {}> = StrapiItem<{
-	publishedAt: string
-} & T>
-
-export type StrapiImage = { //not a publishable item!
-	name: string,
-	alternativeText: string | null,
-	caption: string | null,
-	width: number,
-	height: number,
-	formats: {
-		thumbnail: StrapiImageFormat,
-		small: StrapiImageFormat & { width: 500 },
-		medium: StrapiImageFormat & { width: 750 },
-		large: StrapiImageFormat & { width: 1000 }
+export declare namespace Strapi {
+	type Request = {
+		populate: number | string,
+		[key: string]: any
 	}
-	hash: string,
-	ext: string,
-	mime: string,
-	size: string,
-	url: string,
-	previewUrl: string | null,
-	provider: string,
-	provider_metadata: string | null,
-	createdAt: string,
-	updatedAt: string
-}
 
-export type StrapiImageFormat = {
-	name: string,
-	hash: string,
-	ext: string,
-	mime: string,
-	path: string | null,
-	width: number,
-	height: number,
-	size: number,
-	url: string
+	type Response<T = {}> = PublishableItem<T> //response is a generic item, containing items within it
+
+	type Item<T> = {
+		data: {
+			id: number,
+			attributes: {
+				createdAt: string,
+				updatedAt: string
+			} & T
+		},
+		meta: {}
+	}
+
+	type PublishableItem<T = {}> = Item<{
+		publishedAt: string
+	} & T>
+
+	type Image = { //not a publishable item!
+		name: string,
+		alternativeText: string | null,
+		caption: string | null,
+		width: number,
+		height: number,
+		formats: {
+			thumbnail: ImageFormat,
+			small: ImageFormat & { width: 500 },
+			medium: ImageFormat & { width: 750 },
+			large: ImageFormat & { width: 1000 }
+		}
+		hash: string,
+		ext: string,
+		mime: string,
+		size: string,
+		url: string,
+		previewUrl: string | null,
+		provider: string,
+		provider_metadata: string | null,
+		createdAt: string,
+		updatedAt: string
+	}
+
+	type ImageFormat = {
+		name: string,
+		hash: string,
+		ext: string,
+		mime: string,
+		path: string | null,
+		width: number,
+		height: number,
+		size: number,
+		url: string
+	}
 }
